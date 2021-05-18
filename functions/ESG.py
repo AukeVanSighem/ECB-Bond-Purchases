dbbf87ed451d1087fd798f895f83957dce48afb7
Auke Van Sighem <auke@xki.be>
2021-05-18 09:27:22 +0200

    ESG functions in separate file: DONE

--- functions/ESG.py
+++ functions/ESG.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def cleaning_esg_data(df):
     column_names_esg_company_data = ["ISSUER", "ESG Score 2015", "ESG Score 2016", "ESG Score 2017", 
@@ -55,5 +56,86 @@ def plot_ESG_error_bars(esg_company_data_eligible, esg_company_data_holdings):
     plt.errorbar(years, mean_eligible, yerr=error_eligible, ecolor='y', capsize=10, label="eligible")
 
     plt.legend()
+
+    plt.show()
+
+def linReg_esg_company_data(esg_company_data):
+    list_of_coef = []
+    list_of_intercepts = []
+    years = np.arange(0,7)
+    # We will use the module Linear Regression of sklearn to perform the analysis
+    # Initialize the model
+    ols = LinearRegression()
+    # Fit the model to the data
+    for index,row in esg_company_data.iterrows():
+        resultOfFit = ols.fit(years.reshape(-1, 1),row[1:8])
+        list_of_coef.append(resultOfFit.coef_[0])
+        list_of_intercepts.append(resultOfFit.intercept_)
+    return list_of_coef,list_of_intercepts
+
+def get_list_of_coef_holdings(esg_company_data_eligible, esg_company_data_holdings):
+    list_of_coef_holdings, list_of_intercepts_holdings = linReg_esg_company_data(esg_company_data_holdings)
+    list_of_coef_eligible, list_of_intercepts_eligible = linReg_esg_company_data(esg_company_data_eligible)
+    return list_of_coef_holdings, list_of_intercepts_holdings, list_of_coef_eligible, list_of_intercepts_eligible
+
+def coefficients_box_plot(esg_company_data_eligible, esg_company_data_holdings):
+    list_of_coef_holdings, list_of_intercepts_holdings, list_of_coef_eligible, list_of_intercepts_eligible = get_list_of_coef_holdings(esg_company_data_eligible, esg_company_data_holdings)
+    plt.figure()
+
+    plt.boxplot([list_of_coef_holdings, list_of_coef_eligible], "y")
+    plt.xticks(ticks=[1,2], labels=["holdings", "eligible"])
+
+    plt.show()
+
+def covariance_matrix_plot(esg_company_data_eligible, esg_company_data_holdings):
+    list_of_coef_holdings, list_of_intercepts_holdings, list_of_coef_eligible, list_of_intercepts_eligible = get_list_of_coef_holdings(esg_company_data_eligible, esg_company_data_holdings)
+    plt.figure()
+
+    plt.plot(list_of_intercepts_holdings,list_of_coef_holdings, 'b.')
+    plt.plot(list_of_intercepts_eligible,list_of_coef_eligible, 'y.')
+
+    plt.show()
+
+def get_correlation_coefficients(esg_company_data_eligible, esg_company_data_holdings):
+    list_of_coef_holdings, list_of_intercepts_holdings, list_of_coef_eligible, list_of_intercepts_eligible = get_list_of_coef_holdings(esg_company_data_eligible, esg_company_data_holdings)
+    
+    print("holdings:\n", np.corrcoef(list_of_coef_holdings, list_of_intercepts_holdings),
+      "\n eligible:\n", np.corrcoef(list_of_coef_eligible, list_of_intercepts_eligible))
+
+def get_ESG_scores_per_year(years_issuer_bought, esg_company_data_holdings):
+    esg_company_data_holdings_with_years = years_issuer_bought.merge(esg_company_data_holdings,"inner","ISSUER")
+
+    esg_scores_per_year = {'2015':[],'2016':[],'2017':[],'2018':[],'2019':[],'2020':[],'2021':[]}
+
+    for index,row in esg_company_data_holdings_with_years.iterrows():
+        years = row["YEARS"]
+        for year in years:
+            year = str(year)
+            esg_scores_per_year[year].append(row["ESG Score "+year])
+    return esg_scores_per_year
+
+def get_average_and_std_esg_per_year(years_issuer_bought, esg_company_data_holdings):
+    average_esg_per_year = []
+    std_esg_per_year = []
+    esg_scores_per_year = get_ESG_scores_per_year(years_issuer_bought, esg_company_data_holdings)
+    for key in esg_scores_per_year.keys():
+        average_esg_per_year.append(np.mean(esg_scores_per_year[key]))
+        std_esg_per_year.append(np.std(esg_scores_per_year[key]))
+    return average_esg_per_year, std_esg_per_year
+
+def average_company_esg_score_plot(years_issuer_bought, esg_company_data_holdings):
+    average_esg_per_year, std_esg_per_year = get_average_and_std_esg_per_year(years_issuer_bought, esg_company_data_holdings)
+
+    plt.figure()
+
+    years = range(2015,2022)
+
+    plt.errorbar(x=years,y=average_esg_per_year,yerr=std_esg_per_year,capsize=10)
+
+    plt.title("Average ESG score of the companies that the ECB invested in during that year\n")
+    plt.xlabel("Years")
+    plt.ylabel("ESG score")
+    plt.xticks(years)
+    plt.ylim(0,100)
 
     plt.show()
